a26758ef84697d02495ab8942d5f66db0bce6091
LuuMinhNhat <nguoidep433@gmail.com>
2024-03-01 09:00:19 +0700

    Update bird.dart

--- lib/components/bird.dart
+++ lib/components/bird.dart
@@ -3,7 +3,7 @@ import 'package:flappybird_flutter/game/assets.dart';
 import 'package:flappybird_flutter/game/bird_movement.dart';
 import 'package:flappybird_flutter/game/flappybird_game.dart';
 
-class Bird extends SpriteGroupComponent<bird_movement> with HasGameRef<FlappybirdGame>{
+class Bird extends SpriteGroupComponent<Bird_movement> with HasGameRef<FlappybirdGame>{
   Bird();
   @override
   Future<void> onLoad() async{
@@ -12,11 +12,11 @@ class Bird extends SpriteGroupComponent<bird_movement> with HasGameRef<Flappybir
     final birdDownFlap = await gameRef.loadSprite(Assets.birdDownFlap);
     size = Vector2(50,40);
     position = Vector2(70, gameRef.size.y/2 -size.y/2);
-    current = bird_movement.middle;
+    current = Bird_movement.middle;
     sprites = {
-      bird_movement.middle: birdMidflap,
-      bird_movement.down:birdDownFlap,
-      bird_movement.up:birdUpFlap
+      Bird_movement.middle: birdMidflap,
+      Bird_movement.down:birdDownFlap,
+      Bird_movement.up:birdUpFlap
     };
   }
 
