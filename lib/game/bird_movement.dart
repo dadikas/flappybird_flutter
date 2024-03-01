32e1ff4c3fe2a5d429154847c4b9cc1a8e248405
LuuMinhNhat <nguoidep433@gmail.com>
2024-03-01 08:29:55 +0700

    Revert "bird"
    This reverts commit 18e7f169e0d78678021e68358a36fb6de6910a89.

--- lib/game/bird_movement.dart
+++
@@ -1,1 +0,0 @@
-enum bird_movement {middle,up,down}
--- lib/game/flappybird_game.dart
+++ lib/game/flappybird_game.dart
@@ -1,16 +1,13 @@
 import 'package:flame/game.dart';
 import 'package:flappybird_flutter/components/background.dart';
 import 'package:flappybird_flutter/components/ground.dart';
-import 'package:flappybird_flutter/components/bird.dart';
+
 class FlappybirdGame extends FlameGame {
-  late Bird bird;
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
-      bird = Bird()
     ]);
   }
 }
-
