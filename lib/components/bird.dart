dd58178b1491bad6f1f2849cac8a102599f2a76e
dadikas <73420676+dadikas@users.noreply.github.com>
2024-03-06 10:45:27 +0700

    Revert "Squashed commit of the following:"
    This reverts commit 5886a560eff0bcc86b22c30bda8da328ddac153b.

--- lib/components/bird.dart
+++ lib/components/bird.dart
@@ -5,22 +5,26 @@ import 'package:flappybird_flutter/game/assets.dart';
 import 'package:flappybird_flutter/game/bird_movement.dart';
 import 'package:flappybird_flutter/game/configuration.dart';
 import 'package:flappybird_flutter/game/flappybird_game.dart';
+import 'package:flutter/cupertino.dart';
 
-class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<FlappybirdGame>{
+class Bird extends SpriteGroupComponent<BirdMovement> with
+    HasGameRef<FlappybirdGame>, CollisionCallbacks{
   Bird();
   @override
-  Future<void> onLoad() async {
+  Future<void> onLoad() async{
     final birdMidflap = await gameRef.loadSprite(Assets.birdMidFlap);
     final birdUpFlap = await gameRef.loadSprite(Assets.birdUpFlap);
     final birdDownFlap = await gameRef.loadSprite(Assets.birdDownFlap);
+
+    gameRef.bird;
+
     size = Vector2(50,40);
     position = Vector2(70, gameRef.size.y/2 -size.y/2);
     current = BirdMovement.middle;
-
     sprites = {
       BirdMovement.middle: birdMidflap,
-      BirdMovement.down: birdDownFlap,
-      BirdMovement.up: birdUpFlap
+      BirdMovement.down:birdDownFlap,
+      BirdMovement.up:birdUpFlap
     };
 
     add(CircleHitbox());
@@ -37,10 +41,21 @@ class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<Flappybird
   );
   current =  BirdMovement.up;
  }
+  @override
+  void onCollisionStart(Set<Vector2> intersectionPoints,
+                        PositionComponent other){
+
+    super.onCollisionStart(intersectionPoints, other);
+    debugPrint('collision detected');
+    gameOver();
+  }
+  void gameOver(){
+    gameRef.pauseEngine();
+  }
 
   @override
-  void update(double dt) {
+  void update(double dt){
     super.update(dt);
-    position.y += Config.birdVerlocity * dt;
+    position.y  += Config.birdVerlocity *  dt;
   }
 }
