3a44aaf17525f8ca29d9e98ee3d2122536d77d0e
dadikas <73420676+dadikas@users.noreply.github.com>
2024-03-06 11:13:49 +0700

    update

--- lib/components/bird.dart
+++ lib/components/bird.dart
@@ -7,24 +7,17 @@ import 'package:flappybird_flutter/game/configuration.dart';
 import 'package:flappybird_flutter/game/flappybird_game.dart';
 import 'package:flutter/cupertino.dart';
 
-<<<<<<< HEAD
 class Bird extends SpriteGroupComponent<BirdMovement> with
     HasGameRef<FlappybirdGame>, CollisionCallbacks{
-=======
-class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<FlappybirdGame>{
->>>>>>> parent of 280fefe (make clean code to previous commit)
   Bird();
   @override
   Future<void> onLoad() async{
     final birdMidflap = await gameRef.loadSprite(Assets.birdMidFlap);
     final birdUpFlap = await gameRef.loadSprite(Assets.birdUpFlap);
     final birdDownFlap = await gameRef.loadSprite(Assets.birdDownFlap);
-<<<<<<< HEAD
 
     gameRef.bird;
 
-=======
->>>>>>> parent of 280fefe (make clean code to previous commit)
     size = Vector2(50,40);
     position = Vector2(70, gameRef.size.y/2 -size.y/2);
     current = BirdMovement.middle;
@@ -48,7 +41,6 @@ class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<Flappybird
   );
   current =  BirdMovement.up;
  }
-<<<<<<< HEAD
   @override
   void onCollisionStart(Set<Vector2> intersectionPoints,
                         PositionComponent other){
@@ -60,8 +52,6 @@ class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<Flappybird
   void gameOver(){
     gameRef.pauseEngine();
   }
-=======
->>>>>>> parent of 280fefe (make clean code to previous commit)
 
   @override
   void update(double dt){
